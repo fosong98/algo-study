f0abc443abb93b5873cc88dd7927c74d2f965412
송재원 <fosong98@gmail.com>
2024-01-23 09:52:29 +0900

    <제목>: BOJ_12015 3번째 풀이
    <결과>: 성공
    
    <풀이방법>
    lower bound를이용한 풀이

--- 1주차/class5/BOJ_12015.java
+++ 1주차/class5/BOJ_12015.java
@@ -1,60 +1,58 @@
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(reader.readLine());
 
-        Memory memory = new Memory(N);
-
+        int[] coll = new int[N + 1];
         StringTokenizer st = new StringTokenizer(reader.readLine());
-        while (N-- > 0) {
-            memory.add(Integer.parseInt(st.nextToken()));
-        }
 
-        System.out.println(memory.getLongestSequenceLength());
+        coll[1] = Integer.parseInt(st.nextToken());
+        int maxLength = 1;
+
+        while (st.hasMoreTokens()) {
+            int next = Integer.parseInt(st.nextToken());
+
+            if (coll[maxLength] < next) {
+                coll[++maxLength] = next;
+            } else {
+                int l = binarySearch(coll, maxLength, next);
+                if (l != -1)
+                    coll[l] = next;
+            }
+        }
+        System.out.println(maxLength);
     }
 
-    static class Memory {
-        int[][] coll;
-        int N;
-        int next = 0;
-        int tail = Integer.MAX_VALUE;
+    public static int binarySearch(int[] coll, int length, int number) {
+        int min = 1;
+        int max = length + 1;
 
-        Memory(int N) {
-            this.coll = new int[N][2];
-            this.N = N;
+        int cur = (min + max) / 2;
+        int prev = Integer.MAX_VALUE;
+
+        while (cur != prev) {
+            int curLengthValue = coll[cur];
+            int prevLengthValue = coll[cur - 1];
+
+            if (curLengthValue == number)
+                return -1;
+
+            if (prevLengthValue < number && number < curLengthValue)
+                return cur;
+
+            prev = cur;
+            if (number > curLengthValue) {
+                min = cur;
+                cur = (cur + max) / 2;
+            } else {
+                max = cur;
+                cur = (cur + min) / 2;
+            }
         }
 
-        void add(int num) {
-
-            if (tail > num) {
-                coll[next][0] = num;
-                coll[next][1] = 1;
-                tail = num;
-                next++;
-                return;
-            }
-
-            for (int i = 0; i < next; ++i) {
-                if (coll[i][0] < num) {
-                    coll[i][0] = num;
-                    coll[i][1]++;
-                }
-            }
-
-            coll[next][0] = tail;
-            coll[next][1] = 1;
-            next++;
-        }
-
-        int getLongestSequenceLength() {
-            int max = 0;
-            for (int i = 0; i < next; ++i) {
-                max = Math.max(max, coll[i][1]);
-            }
-            return max;
-        }
+        return -1;
     }
 }
